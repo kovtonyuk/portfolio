c01905dbe5f4a38147ecf312d912c33274ed3195
kovtonyuk <s.kovtonyuk@gmail.com>
2018-07-04 17:17:30 +0300

    add carousel testim section

--- source/js/carousel.js
+++ source/js/carousel.js
@@ -9,3 +9,28 @@ function rotateClass() {
 setInterval(function () {
     rotateClass();
 }, 5000);
+
+
+
+$('.testim__item > div:first-child').addClass('isActive');
+
+function rotateClassTestim() {
+    $('.testim__item > .isActive').appendTo('.testim__item').removeClass('isActive');
+    $('.testim__item > div:first-child').addClass('isActive');
+}
+// Set the Delay Time
+setInterval(function () {
+    rotateClassTestim();
+}, 5000);
+
+
+$('.testim__item--text > div:first-child').addClass('isActive');
+
+function rotateClassTestimText() {
+    $('.testim__item--text > .isActive').appendTo('.testim__item--text').removeClass('isActive');
+    $('.testim__item--text > div:first-child').addClass('isActive');
+}
+// Set the Delay Time
+setInterval(function () {
+    rotateClassTestimText();
+}, 5000);
