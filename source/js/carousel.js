87e5e4aa1dd8d6088229effa95504938223b5a94
kovtonyuk <s.kovtonyuk@gmail.com>
2018-07-04 17:57:24 +0300

    add simple carousel testimonials section

--- source/js/carousel.js
+++ source/js/carousel.js
@@ -1,36 +1,11 @@
-$('.about__item > div:first-child').addClass('isActive');
+function slider(cssClass, timing) {
+    $(cssClass + ' > div:first-child').addClass('isActive');
 
-function rotateClass() {
-    $('.about__item > .isActive').appendTo('.about__item').removeClass('isActive');
-    $('.about__item > div:first-child').addClass('isActive');
+    setInterval(function () {
+        $(cssClass + ' .isActive').appendTo(cssClass).removeClass('isActive');
+        $(cssClass + ' > div:first-child').addClass('isActive');
+    }, timing);
 }
 
-// Set the Delay Time
-setInterval(function () {
-    rotateClass();
-}, 5000);
-
-
-
-$('.testim__item > div:first-child').addClass('isActive');
-
-function rotateClassTestim() {
-    $('.testim__item > .isActive').appendTo('.testim__item').removeClass('isActive');
-    $('.testim__item > div:first-child').addClass('isActive');
-}
-// Set the Delay Time
-setInterval(function () {
-    rotateClassTestim();
-}, 5000);
-
-
-$('.testim__item--text > div:first-child').addClass('isActive');
-
-function rotateClassTestimText() {
-    $('.testim__item--text > .isActive').appendTo('.testim__item--text').removeClass('isActive');
-    $('.testim__item--text > div:first-child').addClass('isActive');
-}
-// Set the Delay Time
-setInterval(function () {
-    rotateClassTestimText();
-}, 5000);
+slider('.about__item', 5000);
+slider('.testim__list', 4000);
